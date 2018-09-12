fcfdda279fabe5ae67d64c1ed19e64168b0a20fd
Ivan Ha <ivanha@hk01.com>
2018-09-12 19:15:45 +0800

    feat: helper to generate dummy list data

---
+++ src/helpers/generateFakeList.js
@@ -0,0 +1,12 @@
+import faker from "faker"
+
+// set a seed for consistent results
+faker.seed(1)
+const genders = ["M", "F"]
+
+export const generateFakeList = size =>
+  new Array(size).fill(undefined).map(() => ({
+    name: faker.name.firstName(),
+    country: faker.address.country(),
+    gender: faker.random.arrayElement(genders),
+  }))

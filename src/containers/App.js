ce56dd5db056453970073f39eac23a40771a1ad3
Ivan Ha <ivanha@hk01.com>
2018-09-13 11:09:19 +0800

    feat: add input field to trigger re-render

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,34 @@
-import { Col, Row } from 'antd'
+import { Col, Input, Row } from 'antd'
 import React, { Component } from 'react'
 
 import List from '../components/List'
 import { generateFakeList } from '../helpers/generateFakeList'
 
+const list = generateFakeList(100)
+
 class App extends Component {
+  state = {
+    data: list,
+    value: '',
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
   render() {
-    const data = generateFakeList(100)
+    const { data, value } = this.state
 
     return (
       <Row>
         <Col span={8} offset={8}>
+          <Input
+            placeholder="Enter something"
+            value={value}
+            onChange={this.handleInputChange}
+          />
           <List data={data} />
         </Col>
       </Row>

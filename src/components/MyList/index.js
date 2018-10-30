401a49efbedf77d153bf25542888e5ef9b48c3a2
Ivan Ha <ivanha@hk01.com>
2018-10-30 13:10:18 +0800

    chore: simplify app for demo

--- src/components/MyList/index.js
+++ src/components/MyList/index.js
@@ -1,3 +1,1 @@
 export { default as MyList } from './List'
-export { default as PureList } from './PureList'
-export { default as PureListItemList } from './PureListItemList'
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,14 @@
-import { Col, Input, Radio, Row } from 'antd'
+import { Col, Input, Row } from 'antd'
 import React, { Component } from 'react'
 
-import { MyList, PureList, PureListItemList } from '../components/MyList'
+import { MyList } from '../components/MyList'
 import { generateFakeList } from '../helpers/generateFakeList'
 
 const list = generateFakeList(100)
 
-const type = {
-  NO_OPTIMIZATION: 'NO_OPTIMIZATION',
-  PURE_LIST: 'PURE_LIST',
-  PURE_LIST_ITEM: 'PURE_LIST_ITEM',
-}
-
 class App extends Component {
   state = {
     value: '',
-    radio: type.NO_OPTIMIZATION,
-  }
-
-  handleRadioChange = e => {
-    this.setState({
-      radio: e.target.value,
-    })
   }
 
   handleInputChange = e => {
@@ -29,21 +16,6 @@ class App extends Component {
       value: e.target.value,
     })
   }
-
-  renderRadio = () => (
-    <Radio.Group
-      defaultValue={type.NO_OPTIMIZATION}
-      buttonStyle="solid"
-      style={{ marginBottom: 16 }}
-      onChange={this.handleRadioChange}
-    >
-      <Radio.Button value={type.NO_OPTIMIZATION}>No optimization</Radio.Button>
-      <Radio.Button value={type.PURE_LIST_ITEM}>
-        {'Pure <ListItem/>'}
-      </Radio.Button>
-      <Radio.Button value={type.PURE_LIST}>{'Pure <List/>'}</Radio.Button>
-    </Radio.Group>
-  )
 
   renderInput = () => (
     <Input
@@ -54,24 +26,12 @@ class App extends Component {
     />
   )
 
-  renderList = () => {
-    switch (this.state.radio) {
-      case type.NO_OPTIMIZATION:
-        return <MyList data={list} />
-      case type.PURE_LIST_ITEM:
-        return <PureListItemList data={list} />
-      case type.PURE_LIST:
-        return <PureList data={list} />
-      default:
-        return <MyList data={list} />
-    }
-  }
+  renderList = () => <MyList data={list} />
 
   render() {
     return (
       <Row>
         <Col span={8} offset={8}>
-          {this.renderRadio()}
           {this.renderInput()}
           {this.renderList()}
         </Col>
